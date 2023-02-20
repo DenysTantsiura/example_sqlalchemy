5dd8f09e8ffa16cd1d92d3ece939a02fdb4313a7
Denys Tantsiura <ghost0002501@gmail.com>
2023-02-20 19:47:57 +0200

    Added Example on core.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import logging
+from sqlite3 import Error
 # from time import time
 from timeit import default_timer
+
+from sqlalchemy import create_engine
 
 
 logging.basicConfig(level=logging.DEBUG, format='%(threadName)s %(message)s')
@@ -15,6 +18,50 @@ def duration(fun):
         return rez
 
     return inner
+
+
+def create_projects_table(conn):
+    try:
+        c = conn.cursor()
+        c.execute(
+            """CREATE TABLE IF NOT EXISTS projects (
+                id integer PRIMARY KEY,
+                name text NOT NULL,
+                begin_date text,
+                end_date text
+            );"""
+        )
+    except Error as e:
+        print(e)
+
+
+def create_tasks_table(conn):
+    try:
+        c = conn.cursor()
+        c.execute(
+            """CREATE TABLE IF NOT EXISTS tasks (
+                id integer PRIMARY KEY,
+                name text NOT NULL,
+                priority integer,
+                project_id integer NOT NULL,
+                status_id integer NOT NULL,
+                begin_date text NOT NULL,
+                end_date text NOT NULL,
+                FOREIGN KEY (project_id) REFERENCES projects (id)
+            );"""
+        )
+    except Error as e:
+        print(e)
+
+
+# Робота з ORM починається зі створення об'єкта, що інкапсулює доступ до бази даних, 
+# в SQLAlchemy він називається engine:
+engine = create_engine('sqlite:///:memory:', echo=True)  # використовуємо SQLite базу даних у пам'яті
+
+
+
+
+
 
 
 @duration
